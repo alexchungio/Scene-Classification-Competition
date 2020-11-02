188b72d0f1ec940d88eb292cbb06b1d3c406bcb8
alexchung <yonganzhong@outlook.com>
2020-11-02 20:36:46 +0800

    update dataset module

--- data/dataset.py
+++ data/dataset.py
@@ -12,6 +12,7 @@
 
 import os
 import glob
+import pandas as pd
 import pathlib
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -20,6 +21,9 @@ import torch
 
 import torch.utils.data as data
 from torchvision import datasets, models, transforms
+
+from libs.configs import cfgs
+from utils.misc import plt_imshow, plot_image_class
 
 
 class SceneDataset(data.Dataset):
@@ -55,11 +59,11 @@ class SceneDataset(data.Dataset):
 
 class DataProvider(object):
 
-    def __init__(self, images, labels, split_ratio):
+    def __init__(self, images, labels=None, split_ratio=0.0):
 
         self.train_images = images[: int(len(images) * split_ratio)]
-        self.train_labels = labels[: int(len(labels) * split_ratio)]
         self.val_images = images[int(len(images) * split_ratio):]
+        self.train_labels = labels[: int(len(labels) * split_ratio)]
         self.val_labels = labels[int(len(labels) * split_ratio):]
 
         # Data augmentation and normalization for training
@@ -79,7 +83,7 @@ class DataProvider(object):
             ]),
         }
 
-    def data_loader(self, batch_size, phase='train', shuffle=False, num_worker=4):
+    def loader(self, batch_size, phase='train', shuffle=False, num_worker=4):
         assert phase in ['train', 'val']
         if phase == 'train':
             dataset = SceneDataset(self.train_images, self.train_labels, self.data_transforms[phase])
@@ -89,3 +93,62 @@ class DataProvider(object):
             loader = data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_worker)
 
         return loader
+
+
+def data_loader(image_path, label_path=None, batch_size=256, split_ratio=0.9):
+
+    """
+
+    :param image_path:
+    :param label_path:
+    :param phase:
+    :param batch_size:
+    :param split_ratio:
+    :return:
+    """
+
+    images_labels = pd.read_csv(label_path, sep=',', header=0)
+
+    train_labels = images_labels['label']
+
+    # labels_count = train_labels.value_counts(sort=False)
+
+    category = sorted(set(train_labels.values.tolist()))
+
+    class_index = {c: i for i, c in enumerate(category)}
+    index_class = {i: c for i, c in enumerate(category)}
+
+    # shuffle DataFrame
+
+    images = [os.path.join(image_path, img) for img in images_labels['filename'].tolist()]
+    labels = [class_index[c] for c in images_labels['label'].tolist()]
+
+    data_provider = DataProvider(images, labels, split_ratio=split_ratio)
+
+    train_loader = data_provider.loader(batch_size=batch_size, phase='train', shuffle=True, num_worker=4)
+    val_loader = data_provider.loader(batch_size=batch_size, phase='val', shuffle=False, num_worker=4)
+
+    return train_loader, val_loader, class_index, index_class
+
+
+
+def main():
+    plt.ion()  # interactive mode
+
+    dataset_dir = pathlib.Path(cfgs.DATASET_PATH)
+
+    labels_path = list(dataset_dir.glob('./*.csv'))[0]
+
+    train_data_path = os.path.join(cfgs.DATASET_PATH, 'train')
+    test_data_path = os.path.join(cfgs.DATASET_PATH, 'test')
+
+    train_loader, val_loader, class_index, index_class = data_loader(train_data_path, labels_path, batch_size=256, split_ratio=0.9)
+
+    image_tensor, labels_tensor = next(iter(val_loader))
+
+    plot_image_class(image_tensor[:36], labels_tensor[:36], index_class)
+
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -11,12 +11,9 @@
 #-------------------------------------------------------
 
 import os
-import glob
+
 import pathlib
-import numpy as np
 import pandas as pd
-import seaborn as sns
-from PIL import Image
 import matplotlib.pyplot as plt
 
 import torch
@@ -24,21 +21,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.utils.data as data
-from torchvision import datasets, models, transforms
+import torchvision
+from torchvision import models
 
 from libs.configs import cfgs
-from data.dataset import DataProvider
+from data.dataset import DataProvider, data_loader
 from utils.misc import plt_imshow, plot_image_class
 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-
 def main():
-    plt.ion()  # interactive mode
-
-
     dataset_dir = pathlib.Path(cfgs.DATASET_PATH)
 
     labels_path = list(dataset_dir.glob('./*.csv'))[0]
@@ -46,34 +40,13 @@ def main():
     train_data_path = os.path.join(cfgs.DATASET_PATH, 'train')
     test_data_path = os.path.join(cfgs.DATASET_PATH, 'test')
 
-    images_labels = pd.read_csv(labels_path, sep=',', header=0)
-
-    train_labels = images_labels['label']
-
-    labels_count = train_labels.value_counts(sort=False)
-
-
-    category = sorted(set(train_labels.values.tolist()))
-
-    class_index = {c:i for i, c in enumerate(category)}
-    index_class = {i:c for i, c in enumerate(category)}
-
-
-    # shuffle DataFrame
-
-    train_val_images = [os.path.join(train_data_path, img) for img in images_labels['filename'].tolist()]
-    train_val_labels =  [class_index[c] for c in images_labels['label'].tolist()]
-
-    data_loader = DataProvider(train_val_images, train_val_labels, split_ratio=0.9)
-
-    train_loader = data_loader.data_loader(batch_size=256, phase='train', shuffle=True, num_worker=4)
-    val_loader = data_loader.data_loader(batch_size=256, phase='val', shuffle=False, num_worker=4)
-
-    image_tensor, labels_tensor = next(iter(val_loader))
-
-    plot_image_class(image_tensor[:36], labels_tensor[:36], index_class)
-
+    train_loader, val_loader, class_index, index_class = data_loader(train_data_path, labels_path, batch_size=256,
+                                                                     split_ratio=0.9)
 
 
 if __name__ == "__main__":
+
     main()
+
+
+

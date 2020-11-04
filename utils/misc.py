55907ddeb6cf9cb7c179212a7046aceb699cbb50
alexchung <yonganzhong@outlook.com>
2020-11-04 11:50:57 +0800

    add data module

--- utils/misc.py
+++ utils/misc.py
@@ -12,6 +12,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def read_class_names(class_file_name):
+    '''loads class name from a file'''
+    names = {}
+    with open(class_file_name, 'r') as data:
+        for ID, name in enumerate(data):
+            names[ID] = name.strip('\n')
+    return names
+
+
 def plt_imshow(image, title=None):
     """
 
